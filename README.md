aa0b910270693239761f0dddf967ebc61a57b243
Paolo Bellardone <paolo.bellardone@outlook.com>
2020-03-17 14:16:14 +0100

    Update to latest version of fn and Jet

--- README.md
+++ README.md
@@ -4,8 +4,21 @@ This directory contains a Javascript (Node) function to evaluate the Italian Fis
 
 This function has to be deployed on a fn server, see http://fnproject.io
 
+Quickstart:
+1. fn start
+  * Starts a new fn server (not for production)
+2. fn create app cfapp
+  * Create a new app to deploy functions into it
+3. fn deploy --app cfapp --local
+  * Deploy the new app into the fn server
+4. fn inspect function cfapp cf
+  * Get the URL to invoke the function
+
+
 ### ojetcfdemo
 This directory contains a Oracle Jet application that invokes the previous function.
+
+Please update the url parameter in file main.js @ line 53 to reflect the new invoke URL got in the prvious step.
 
 The application can be run standalone using the command: *ojet serve* or can be deployed in a Docker container using the included Dockerfile.
 
--- ojetcfdemo/src/js/main.js
+++ ojetcfdemo/src/js/main.js
@@ -1,42 +1,43 @@
 'use strict';
 
-requirejs.config(
-{
-  baseUrl: 'js',
+ // The UserAgent is used to detect IE11. Only IE11 requires ES5.
+(function () {
+  
+  function _ojIsIE11() {
+    var nAgt = navigator.userAgent;
+    return nAgt.indexOf('MSIE') !== -1 || !!nAgt.match(/Trident.*rv:11./);
+  };
+  var _ojNeedsES5 = _ojIsIE11();
 
-  // Path mappings for the logical module names
-  // Update the main-release-paths.json for release mode when updating the mappings
-  paths:
-  //injector:mainReleasePaths
-  {
-    'knockout': 'libs/knockout/knockout-3.5.0.debug',
-    'jquery': 'libs/jquery/jquery-3.4.1',
-    'jqueryui-amd': 'libs/jquery/jqueryui-amd-1.12.1',
-    'promise': 'libs/es6-promise/es6-promise',
-    'hammerjs': 'libs/hammer/hammer-2.0.8',
-    'ojdnd': 'libs/dnd-polyfill/dnd-polyfill-1.0.0',
-    'ojs': 'libs/oj/v7.2.0/debug',
-    'ojL10n': 'libs/oj/v7.2.0/ojL10n',
-    'ojtranslations': 'libs/oj/v7.2.0/resources',
-    'text': 'libs/require/text',
-    'signals': 'libs/js-signals/signals',
-    'customElements': 'libs/webcomponents/custom-elements.min',
-    'proj4': 'libs/proj4js/dist/proj4-src',
-    'css': 'libs/require-css/css',
-    'touchr': 'libs/touchr/touchr'
-  }
-  //endinjector
-  ,
-  // Shim configurations for modules that do not expose AMD
-  shim:
-  {
-    'jquery':
+  requirejs.config(
     {
-      exports: ['jQuery', '$']
+      baseUrl: 'js',
+
+      // Path mappings for the logical module names
+      paths:
+      // injector:mainReleasePaths
+      {
+        'knockout': 'libs/knockout/knockout-3.5.0.debug',
+        'jquery': 'libs/jquery/jquery-3.4.1',
+        'jqueryui-amd': 'libs/jquery/jqueryui-amd-1.12.1',
+        'hammerjs': 'libs/hammer/hammer-2.0.8',
+        'ojdnd': 'libs/dnd-polyfill/dnd-polyfill-1.0.1',
+        'ojs': 'libs/oj/v8.1.0/debug' + (_ojNeedsES5 ? '_es5' : ''),
+        'ojL10n': 'libs/oj/v8.1.0/ojL10n',
+        'ojtranslations': 'libs/oj/v8.1.0/resources',
+        'text': 'libs/require/text',
+        'signals': 'libs/js-signals/signals',
+        'customElements': 'libs/webcomponents/custom-elements.min',
+        'proj4': 'libs/proj4js/dist/proj4-src',
+        'css': 'libs/require-css/css.min',
+        'touchr': 'libs/touchr/touchr',
+        'corejs' : 'libs/corejs/shim',
+        'regenerator-runtime' : 'libs/regenerator-runtime/runtime'
+      }
+      // endinjector
     }
-  }
-}
-);
+  );
+}());
 
 /**
  * A top-level require call executed by the Application.
@@ -44,33 +45,32 @@ requirejs.config(
  * by the modules themselves), we are listing them explicitly to get the references to the 'oj' and 'ko'
  * objects in the callback
  */
-require(['ojs/ojcore', 'knockout', 'appController', 'ojs/ojknockout',
+require(['ojs/ojbootstrap', 'knockout', 'appController', 'ojs/ojrouter', 'ojs/ojlogger', 'ojs/ojknockout',
   'ojs/ojmodule', 'ojs/ojrouter', 'ojs/ojnavigationlist', 'ojs/ojbutton', 'ojs/ojtoolbar'],
-  function (oj, ko, app) { // this callback gets executed when all required modules are loaded
+  function (Bootstrap, ko, app, Router, Logger) { // this callback gets executed when all required modules are loaded
+    Bootstrap.whenDocumentReady().then(
+      function() {
 
-    $(function() {
+        function init() {
+          Router.sync().then(
+            function () {
+              app.loadModule();
+              // Bind your ViewModel for the content of the whole page body.
+              ko.applyBindings(app, document.getElementById('globalBody'));
+            },
+            function (error) {
+              Logger.error('Error in root start: ' + error.message);
+            }
+          );
+        }
 
-      function init() {
-        oj.Router.sync().then(
-          function () {
-            // Bind your ViewModel for the content of the whole page body.
-            ko.applyBindings(app, document.getElementById('globalBody'));
-          },
-          function (error) {
-            oj.Logger.error('Error in root start: ' + error.message);
-          }
-        );
-      }
-
-      // If running in a hybrid (e.g. Cordova) environment, we need to wait for the deviceready
-      // event before executing any code that might interact with Cordova APIs or plugins.
-      if ($(document.body).hasClass('oj-hybrid')) {
-        document.addEventListener("deviceready", init);
-      } else {
-        init();
-      }
-
-    });
-
+        // If running in a hybrid (e.g. Cordova) environment, we need to wait for the deviceready
+        // event before executing any code that might interact with Cordova APIs or plugins.
+        if (document.body.classList.contains('oj-hybrid')) {
+          document.addEventListener("deviceready", init);
+        } else {
+          init();
+        }
+      });
   }
 );
